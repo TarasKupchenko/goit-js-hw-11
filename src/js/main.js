49566aab06a8de3e4ab3163d3d8b6418db164fa0
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2023-11-18 22:10:36 +0200

    start

--- src/js/main.js
+++ src/js/main.js
@@ -1,129 +1,9 @@
-//import axios from 'axios';
-//import Notiflix from 'notiflix';
-
-//Notiflix.Notify.Init({
-//   width: '300px',
-//   position: 'right-bottom',
-//   fontSize: '16px',
-//   distance: '10px',
-//   borderRadius: '5px',
-//   timeout: 3000,
-// });
-
-//document.addEventListener('DOMContentLoaded', () => {
-//  const form = document.getElementById('search-form');
-//  const gallery = document.getElementById('gallery');
-//  const loadMoreBtn = document.getElementById('load-more');
-//  let page = 1; // Початкова сторінка
-
-//  form.addEventListener('submit', async (event) => {
-//    event.preventDefault();
-
-//    // Очистка галереї перед новим пошуком
-//    gallery.innerHTML = '';
-//    page = 1; // Скидання сторінки при новому пошуку
-
-//    const searchQuery = form.searchQuery.value;
-//    if (searchQuery.trim() !== '') {
-//      // Виклик функції для виконання HTTP-запиту
-//      await performSearch(searchQuery, page);
-//      // Показ кнопки "Load more"
-//      loadMoreBtn.style.display = 'block';
-//    }
-//  });
-
-//  loadMoreBtn.addEventListener('click', async () => {
-//    page++; // Збільшення сторінки при натисканні кнопки "Load more"
-//    const searchQuery = form.searchQuery.value;
-//    await performSearch(searchQuery, page);
-//  });
-
-//  async function performSearch(query, currentPage) {
-//    try {
-//      const apiKey = '40756450-2b62d5efbb9c5d98f0ec642a2'; // Замініть на свій API-ключ Pixabay
-//      const perPage = 40; // Кількість зображень на сторінці
-
-//      const response = await axios.get('https://pixabay.com/api/', {
-//        params: {
-//          key: apiKey,
-//          q: query,
-//          image_type: 'photo',
-//          orientation: 'horizontal',
-//          safesearch: true,
-//          page: currentPage,
-//          per_page: perPage,
-//        },
-//      });
-
-//      const images = response.data.hits;
-//      if (images.length === 0) {
-//        // Вивід повідомлення, якщо результатів не знайдено
-//        alert('Sorry, there are no images matching your search query. Please try again.');
-//      } else {
-//        // Додавання зображень до галереї
-//        images.forEach((image) => {
-//          const card = createPhotoCard(image);
-//          gallery.appendChild(card);
-//        });
-
-//        // Перевірка на кінець колекції
-//        const totalHits = response.data.totalHits || 0;
-//        const totalPages = Math.ceil(totalHits / perPage);
-//        if (currentPage >= totalPages) {
-//          loadMoreBtn.style.display = 'none';
-//          alert("We're sorry, but you've reached the end of search results.");
-//        }
-//      }
-//    } catch (error) {
-//      console.error('Error fetching images:', error);
-//      alert('An error occurred while fetching images. Please try again.');
-//    }
-//  }
-
-//  function createPhotoCard(image) {
-//    const card = document.createElement('div');
-//    card.className = 'photo-card';
-
-//    const img = document.createElement('img');
-//    img.src = image.webformatURL;
-//    img.alt = image.tags;
-//    img.loading = 'lazy';
-
-//    const info = document.createElement('div');
-//    info.className = 'info';
-
-//    const likes = document.createElement('p');
-//    likes.className = 'info-item';
-//    likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
-
-//    const views = document.createElement('p');
-//    views.className = 'info-item';
-//    views.innerHTML = `<b>Views:</b> ${image.views}`;
-
-//    const comments = document.createElement('p');
-//    comments.className = 'info-item';
-//    comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
-
-//    const downloads = document.createElement('p');
-//    downloads.className = 'info-item';
-//    downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
-
-//    info.appendChild(likes);
-//    info.appendChild(views);
-//    info.appendChild(comments);
-//    info.appendChild(downloads);
-
-//    card.appendChild(img);
-//    card.appendChild(info);
-
-//    return card;
-//  }
-//});
-
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-Notiflix.Notify.Init({
+Notiflix.Notify.init({
   width: '300px',
   position: 'right-bottom',
   fontSize: '16px',
@@ -132,100 +12,133 @@ Notiflix.Notify.Init({
   timeout: 3000,
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('search-form');
-  const gallery = document.getElementById('gallery');
-  const loadMoreBtn = document.getElementById('load-more');
-  let page = 1; // Початкова сторінка
+const gallery = document.getElementById('gallery');
+const form = document.getElementById('search-form');
+const loadMoreBtn = document.getElementById('load-more');
+let page = 1; // Початкова сторінка
 
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    // Очистка галереї перед новим пошуком
-    gallery.innerHTML = '';
-    page = 1; // Скидання сторінки при новому пошуку
-
-    const searchQuery = form.searchQuery.value;
-    if (searchQuery.trim() !== '') {
-      try {
-        const images = await performSearch(searchQuery, page);
-
-        if (images.length === 0) {
-          // Вивід повідомлення, якщо результатів не знайдено
-          Notiflix.Notify.Info('Sorry, there are no images matching your search query. Please try again.');
-        } else {
-          // Додавання зображень до галереї
-          images.forEach((image) => {
-            const card = createPhotoCard(image);
-            gallery.appendChild(card);
-          });
-
-          // Показ кнопки "Load more"
-          loadMoreBtn.style.display = 'block';
-
-          // Перевірка на кінець колекції
-          const totalHits = images.totalHits || 0;
-          const totalPages = Math.ceil(totalHits / images.per_page);
-          if (page >= totalPages) {
-            loadMoreBtn.style.display = 'none';
-            Notiflix.Notify.Warning("We're sorry, but you've reached the end of search results.");
-          }
-        }
-      } catch (error) {
-        console.error('Error during search:', error);
-        Notiflix.Notify.Failure('An error occurred during the search. Please try again.');
-      }
-    }
-  });
-
-  loadMoreBtn.addEventListener('click', async () => {
-    page++; // Збільшення сторінки при натисканні кнопки "Завантажити ще"
-    const searchQuery = form.searchQuery.value;
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  gallery.innerHTML = '';
+  page = 1;
+  const searchQuery = form.searchQuery.value.trim();
+  
+  if (searchQuery !== '') {
     try {
-      const images = await performSearch(searchQuery, page);
-      // Додавання зображень до галереї
-      images.forEach((image) => {
-        const card = createPhotoCard(image);
-        gallery.appendChild(card);
-      });
+      const { images, totalHits } = await performSearch(searchQuery, page);
+      displayImages(images);
 
-      // Перевірка на кінець колекції
-      const totalHits = images.totalHits || 0;
-      const totalPages = Math.ceil(totalHits / images.per_page);
-      if (page >= totalPages) {
-        loadMoreBtn.style.display = 'none';
-        Notiflix.Notify.Warning("We're sorry, but you've reached the end of search results.");
-      }
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+
+      loadMoreBtn.style.display = 'block';
     } catch (error) {
-      console.error('Error during "Load more":', error);
-      Notiflix.Notify.Failure('An error occurred while loading more images. Please try again.');
+      //console.error('Error during search:', error);
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     }
-  });
-
-  async function performSearch(query, currentPage) {
-    try {
-      const apiKey = 'YOUR_PIXABAY_API_KEY'; // Замініть на свій ключ Pixabay API
-      const perPage = 40;
-
-      const response = await axios.get('https://pixabay.com/api/', {
-        params: {
-          key: apiKey,
-          q: query,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          page: currentPage,
-          per_page: perPage,
-        },
-      });
-
-      return response.data;
-    } catch (error) {
-      throw error; // Rethrow the error for better handling in the calling function
-    }
-  }
-
-  function createPhotoCard(image) {
-    // ... (unchanged)
   }
 });
+
+loadMoreBtn.addEventListener('click', async () => {
+  page++;
+  const searchQuery = form.searchQuery.value.trim();
+  
+  try {
+    const { images } = await performSearch(searchQuery, page);
+    displayImages(images);
+  } catch (error) {
+    console.error('Error during "Load more":', error);
+    Notiflix.Notify.failure('An error occurred while loading more images. Please try again.');
+  }
+});
+
+async function performSearch(query, currentPage) {
+  try {
+    const apiKey = '40756450-2b62d5efbb9c5d98f0ec642a2'; 
+    const perPage = 40;
+
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: apiKey,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: currentPage,
+        per_page: perPage,
+      },
+    });
+
+    const totalHits = response.data.totalHits || 0;
+    const images = response.data.hits;
+
+    return { images, totalHits };
+  } catch (error) {
+    throw error;
+  }
+}
+
+function displayImages(images) {
+  images.forEach((image) => {
+    const card = createPhotoCard(image);
+    gallery.appendChild(card);
+  });
+
+  // Перевірка на кінець колекції
+  const totalHits = images.totalHits || 0;
+  const totalPages = Math.ceil(totalHits / images.per_page);
+  if (page >= totalPages) {
+    loadMoreBtn.style.display = 'none';
+    Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+  }
+
+  // Виклик методу refresh() бібліотеки SimpleLightbox
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+
+   // Плавне прокручування сторінки
+   const cardHeight = gallery.firstElementChild.getBoundingClientRect().height;
+   window.scrollBy({
+     top: cardHeight * 2,
+     behavior: 'smooth',
+   });
+}
+
+function createPhotoCard(image) {
+  const card = document.createElement('div');
+  card.classList.add('photo-card');
+
+  // Create an anchor element to wrap the image
+  const anchor = document.createElement('a');
+  anchor.href = image.largeImageURL; // Set the larger image URL as the anchor's href
+  anchor.setAttribute('data-lightbox', 'gallery'); // Set data-lightbox attribute for SimpleLightbox
+
+  const img = document.createElement('img');
+  img.src = image.webformatURL;
+  img.alt = image.tags;
+  img.loading = 'lazy';
+
+  // Append the image to the anchor
+  anchor.appendChild(img);
+
+  const info = document.createElement('div');
+  info.classList.add('info');
+
+  const infoItems = [
+    { label: 'Likes', value: image.likes },
+    { label: 'Views', value: image.views },
+    { label: 'Comments', value: image.comments },
+    { label: 'Downloads', value: image.downloads },
+  ];
+
+  infoItems.forEach((item) => {
+    const p = document.createElement('p');
+    p.classList.add('info-item');
+    p.innerHTML = `<b>${item.label}</b>: ${item.value}`;
+    info.appendChild(p);
+  });
+
+  card.appendChild(anchor); // Append the anchor to the card
+  card.appendChild(info);
+
+  return card;
+}
